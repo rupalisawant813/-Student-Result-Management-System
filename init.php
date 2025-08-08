54171c2987a530aabcbe8c3c9b258ba8d4dcca52
Rupali Sawant <rupalisawant813@gmail.com>
2025-08-08 15:00:56 +0530

    Initial commit - Result Management System

---
+++ init.php
@@ -0,0 +1,9 @@
+<?php
+	$servername = "localhost";
+	$username = "root";
+	$password = "Rup@li7522";
+	$conn = mysqli_connect($servername, $username, $password);
+	$db = mysqli_select_db($conn,'srms');
+
+
+?>
